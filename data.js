d81719541a1f3e4c2d9e8f81102cddbb71cc8e26
skyblaster89 <skyblaster89@gmail.com>
2021-01-22 11:50:06 -0600

    Engineering

---
+++ data.js
@@ -0,0 +1,73 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-engineering",
+      "name": "Engineering",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.9241118465987235,
+          "pitch": 0.28868557419134255,
+          "title": "Engineering Intro",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.6119001131925064,
+          "pitch": 0.012472317883309714,
+          "title": "Engineering Video 2",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.1653394252602443,
+          "pitch": 0.2522829796258712,
+          "title": "Engineering 5",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.11993548103065166,
+          "pitch": -0.010090994458156644,
+          "title": "Engineering 4",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.04205505457315084,
+          "pitch": 0.0015901568642533448,
+          "title": "Engineering 3",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Engineering",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
