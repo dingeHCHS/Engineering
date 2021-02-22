2b991353b481b23a48dc82f372b4273270a1c21a
dingeHCHS <69469881+dingeHCHS@users.noreply.github.com>
2021-02-22 11:10:07 -0600

    sum

--- data.js
+++ data.js
@@ -24,49 +24,49 @@ var APP_DATA = {
       ],
       "faceSize": 1520,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -0.01236754223539549,
+        "pitch": 0.0056165670666743495,
+        "fov": 1.3715802068843215
       },
       "linkHotspots": [],
       "infoHotspots": [
         {
-          "yaw": -0.9241118465987235,
-          "pitch": 0.28868557419134255,
+          "yaw": -0.01236754223539549,
+          "pitch": 0.0056165670666743495,
           "title": "Engineering Intro",
-          "text": '<iframe src="https://drive.google.com/file/d/1I4E-x6Y-ojfX9K3x13tfaxr13TevfaqB/preview" width="640" height="480"></iframe>'
+          "text": "Text"
         },
         {
-          "yaw": -0.6119001131925064,
-          "pitch": 0.012472317883309714,
-          "title": "Engineering Video 2",
-          "text": '<iframe src="https://drive.google.com/file/d/1I4FzqgVpE45-D8Iz1ScNPEttcvKke8-b/preview" width="640" height="480"></iframe>'
+          "yaw": 0.4689057808396626,
+          "pitch": 0.040489935390855436,
+          "title": "E1",
+          "text": "Text"
         },
         {
-          "yaw": 2.1653394252602443,
-          "pitch": 0.2522829796258712,
-          "title": "Engineering 5",
-          "text": '<iframe src="https://drive.google.com/file/d/1IBgzWWsrayCwlfgzJCfcw062k7fGqcgW/preview" width="640" height="480"></iframe>'
+          "yaw": 0.9705349111283343,
+          "pitch": 0.03233748245680168,
+          "title": "E2",
+          "text": "Text"
         },
         {
-          "yaw": 0.11993548103065166,
-          "pitch": -0.010090994458156644,
-          "title": "Engineering 4",
-          "text": '<iframe src="https://drive.google.com/file/d/1IG9WxfzC-LNsnVIDKu1zCT7U9CMFnt2y/preview" width="640" height="480"></iframe>'
+          "yaw": -0.6017247072472607,
+          "pitch": 0.03534426682760383,
+          "title": "E3",
+          "text": "Text"
         },
         {
-          "yaw": -0.04205505457315084,
-          "pitch": 0.0015901568642533448,
-          "title": "Engineering 3",
-          "text": '<iframe src="https://drive.google.com/file/d/1I5bGNlpe4oZUkiBscpVI92kbiE01AOFb/preview" width="640" height="480"></iframe>'
+          "yaw": -1.175976475853476,
+          "pitch": 0.023363753278340837,
+          "title": "E4",
+          "text": "Text"
         }
       ]
     }
   ],
-  "name": "Engineering",
+  "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": true,
+    "autorotateEnabled": false,
     "fullscreenButton": false,
     "viewControlButtons": false
   }
