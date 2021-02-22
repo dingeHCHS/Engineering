8dead153f6f05e2d2b8c91993d980141b57cf92a
dingeHCHS <69469881+dingeHCHS@users.noreply.github.com>
2021-02-22 11:12:57 -0600

    Update data.js

--- data.js
+++ data.js
@@ -34,31 +34,31 @@ var APP_DATA = {
           "yaw": -0.01236754223539549,
           "pitch": 0.0056165670666743495,
           "title": "Engineering Intro",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1IBgzWWsrayCwlfgzJCfcw062k7fGqcgW/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": 0.4689057808396626,
           "pitch": 0.040489935390855436,
           "title": "E1",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1IG9WxfzC-LNsnVIDKu1zCT7U9CMFnt2y/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": 0.9705349111283343,
           "pitch": 0.03233748245680168,
           "title": "E2",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1I5bGNlpe4oZUkiBscpVI92kbiE01AOFb/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": -0.6017247072472607,
           "pitch": 0.03534426682760383,
           "title": "E3",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1I4FzqgVpE45-D8Iz1ScNPEttcvKke8-b/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": -1.175976475853476,
           "pitch": 0.023363753278340837,
           "title": "E4",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1I4E-x6Y-ojfX9K3x13tfaxr13TevfaqB/preview" width="640" height="480"></iframe>'
         }
       ]
     }
